07f93a2a170aae36730f06b6161e5df3b9e4200c
Alex Stuckey <a.stuckey@auckland.ac.nz>
2016-01-20 11:11:35 +1300

    Add package loading code
    Load all the packages you want without having to type library() for
    each one.
    
    Only doing it for R packages at the moment.

--- transcriptome_panel/server.R
+++ transcriptome_panel/server.R
@@ -7,7 +7,8 @@ list_of_packages <- c("shiny")
 new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
 if(length(new_packages)) install.packages(new_packages)
 
-library(shiny)
+invisible(lapply(list_of_packages, library, character.only = TRUE))
+
 library(cummeRbund)
 
 
